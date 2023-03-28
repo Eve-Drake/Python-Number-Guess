72f53214de12a3d88227c80827473e7224aea96e
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2023-03-28 11:44:21 +0100

    Computer number guess final

--- Guess-Computers-Number.py
+++ Guess-Computers-Number.py
@@ -4,11 +4,11 @@ def guess(x):
     ranNum = random.randint(1, x)
     userGuess = 0
     while userGuess != ranNum:
-        userGuess = input('Guess a number...')
+        userGuess = int(input('Guess a number...'))
         print(f'You guessed {userGuess}')
-        if int(userGuess) > ranNum:
+        if userGuess > ranNum:
             print('That is too high')
-        elif int(userGuess) < ranNum:
+        elif userGuess < ranNum:
             print('That is too low')
     print(f"Correct! The number was {ranNum}")
 guess(100)
